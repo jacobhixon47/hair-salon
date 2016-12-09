c7a659de08015189ee493a9e57e48f1655bf2845
Jacob Hixon <jacobhixon47@gmail.com>
2016-12-08 22:33:38 -0800

    pass Stylist spec for showing name

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,7 +1,7 @@
 class Stylist
   attr_reader(:name, :id)
 
-  define_method(:initialize) do
+  define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -3,10 +3,15 @@ require('spec_helper')
 describe(Stylist) do
   describe('.all') do
     it('starts off with no stylists') do
-      expect(Client.all()).to(eq([]))
+      expect(Stylist.all()).to(eq([]))
     end
   end
 
-  
+  describe("#name") do
+    it("tells you the stylists") do
+      stylist = Stylist.new({:name => "Jacob", :id => nil})
+      expect(stylist.name()).to(eq("Jacob"))
+    end
+  end
 
 end
