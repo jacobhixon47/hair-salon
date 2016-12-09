e5a6ba884206fa7044c51fcb12f45f38a656036e
Jacob Hixon <jacobhixon47@gmail.com>
2016-12-08 22:47:50 -0800

    pass Stylist specs for showing id, saving it to database and comparing two

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -11,9 +11,18 @@ class Stylist
     stylists = []
     returned_stylists.each() do |stylist|
       name = stylist.fetch('name')
-      id = stylist.fetch('id')
+      id = stylist.fetch('id').to_i()
       stylists.push(Stylist.new({:name => name, :id => id}))
     end
     stylists
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch('id').to_i()
+  end
+
+  define_method(:==) do |another_stylist|
+    self.name() == another_stylist.name() && self.id() == another_stylist.id()
   end
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -7,10 +7,34 @@ describe(Stylist) do
     end
   end
 
-  describe("#name") do
-    it("tells you the stylists") do
+  describe('#name') do
+    it('tells you the stylists') do
       stylist = Stylist.new({:name => "Jacob", :id => nil})
       expect(stylist.name()).to(eq("Jacob"))
+    end
+  end
+
+  describe('#id') do
+    it('sets its ID when you save the stylist') do
+      stylist = Stylist.new({:name => 'Jacob', :id => nil})
+      stylist.save()
+      expect(stylist.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#save') do
+    it('allows you to save stylists to the database') do
+      stylist = Stylist.new({:name => 'Jacob', :id => nil})
+      stylist.save()
+      expect(Stylist.all()).to(eq([stylist]))
+    end
+  end
+
+  describe('#==') do
+    it('is the same stylist if it has the same name') do
+      stylist = Stylist.new({:name => 'Jacob', :id => nil})
+      stylist2 = Stylist.new({:name => 'Jacob', :id => nil})
+      expect(stylist).to(eq(stylist2))
     end
   end
 
