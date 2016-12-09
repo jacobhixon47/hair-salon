d544da9bd1643b65b8efe18c61aa2064577a2ceb
Jacob Hixon <jacobhixon47@gmail.com>
2016-12-08 22:55:54 -0800

    add stylist_id to Client class

--- lib/client.rb
+++ lib/client.rb
@@ -1,13 +1,14 @@
 class Client
-  attr_reader(:first_name, :last_name)
+  attr_reader(:first_name, :last_name, :stylist_id)
 
   define_method(:initialize) do |attributes|
     @first_name = attributes.fetch(:first_name)
     @last_name = attributes.fetch(:last_name)
+    @stylist_id = attributes.fetch(:stylist_id)
   end
 
   define_method(:==) do |another_client|
-    self.first_name() == another_client.first_name() && self.last_name() == another_client.last_name()
+    self.first_name() == another_client.first_name() && self.last_name() == another_client.last_name() && self.stylist_id() == another_client.stylist_id()
   end
 
   define_singleton_method(:all) do
@@ -16,12 +17,13 @@ class Client
     returned_clients.each() do |client|
       first_name = client.fetch('first_name')
       last_name = client.fetch('last_name')
-      clients.push(Client.new({:first_name => first_name, :last_name => last_name}))
+      stylist_id = client.fetch('stylist_id').to_i*
+      clients.push(Client.new({:first_name => first_name, :last_name => last_name, :stylist_id => stylist_id}))
     end
     clients
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO clients (first_name, last_name) VALUES ('#{@first_name}', '#{@last_name}');")
+    DB.exec("INSERT INTO clients (first_name, last_name, stylist_id) VALUES ('#{@first_name}', '#{@last_name}', #{@stylist_id});")
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -3,8 +3,8 @@ require('spec_helper')
 describe(Client) do
   describe('#==') do
     it('is the same client if it has the same first name and last name') do
-      client1 = Client.new({first_name: 'John', last_name: 'Smith'})
-      client2 = Client.new({first_name: 'John', last_name: 'Smith'})
+      client1 = Client.new({first_name: 'John', last_name: 'Smith', stylist_id: 1})
+      client2 = Client.new({first_name: 'John', last_name: 'Smith', stylist_id: 1})
       expect(client1).to(eq(client2))
     end
   end
@@ -17,7 +17,7 @@ describe(Client) do
 
   describe('#save') do
     it('adds a client to the database') do
-      test_client = Client.new({first_name: 'John', last_name: 'Smith'})
+      test_client = Client.new({first_name: 'John', last_name: 'Smith', stylist_id: 1})
       test_client.save()
       expect(Client.all()).to(eq([test_client]))
     end
