bea86ee4ca781c4b4d095f3fb18a0994f8b98a85
Jacob Hixon <jacobhixon47@gmail.com>
2016-12-08 22:07:27 -0800

    pass Client class spec for .all method

--- lib/client.rb
+++ lib/client.rb
@@ -1,5 +1,5 @@
 class Client
-  attr_reader(:first_name, :last_name, :id)
+  attr_reader(:first_name, :last_name)
 
   define_method(:initialize) do |attributes|
     @first_name = attributes.fetch(:first_name)
@@ -8,5 +8,16 @@ class Client
 
   define_method(:==) do |another_client|
     self.first_name() == another_client.first_name() && self.last_name() == another_client.last_name()
+  end
+
+  define_singleton_method(:all) do
+    returned_clients = DB.exec('SELECT * FROM clients;')
+    clients = []
+    returned_clients.each() do |client|
+      first_name = client.fetch('first_name')
+      last_name = client.fetch('last_name')
+      clients.push(Client.new({:first_name => first_name, :last_name => last_name}))
+    end
+    clients
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -8,4 +8,11 @@ describe(Client) do
       expect(client1).to(eq(client2))
     end
   end
+
+  describe('.all') do
+    it('is empty at first') do
+      expect(Client.all()).to(eq([]))
+    end
+  end
+  
 end
